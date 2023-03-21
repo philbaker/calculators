54d7040752dc668ecaffea3f353cfcd70e54a9ec
philbaker <phil@philbaker.me>
2023-03-21 22:02:57 +0000

    Add active links

--- src/main/calculators/routes.cljs
+++ src/main/calculators/routes.cljs
@@ -16,26 +16,35 @@
      :view views/percentage-calculator}]
    ["/color"
     {:name ::color
-     :view views/color-calculator}]
-   ])
+     :view views/color-calculator}]])
 
 (defn header []
   [:div.px-6
    [:h1.text-2xl.mt-3 "Calculators"]])
 
+(defn active-link [route]
+  (when (= (:name (:data @match)) route) "underline"))
+
 (defn current-page []
   [:div
    [header]
    [:ul {:class "p-6 flex space-x-4"}
-    [:li [:a {:class "text-md text-blue-800 hover:underline"
-              :href (rfe/href ::pixel)} 
-          "Pixel"]]
-    [:li [:a {:class "text-md text-blue-800 hover:underline"
-              :href (rfe/href ::percentage)} 
-          "Percentage"]]
-    [:li [:a {:class "text-md text-blue-800 hover:underline"
-              :href (rfe/href ::color)} 
-          "Colour"]]]
+    [:li 
+     [:a 
+      {:class (str "text-md text-blue-800 hover:underline " (active-link :calculators.routes/pixel))
+       :href (rfe/href ::pixel)} 
+      "Pixel"]]
+    [:li 
+     [:a 
+      {:class (str "text-md text-blue-800 hover:underline " (active-link :calculators.routes/percentage))
+       :href (rfe/href ::percentage)} 
+      "Percentage"]]
+    [:li 
+     [:a 
+      {:class (str "text-md text-blue-800 hover:underline " (active-link :calculators.routes/color))
+       :href (rfe/href ::color)} 
+      "Colour"]]]
+   
    (if @match
      (let [view (:view (:data @match))]
        [view @match]))])
